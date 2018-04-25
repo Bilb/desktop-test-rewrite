whycoding126/Signal-Desktop

Separate required from optional `Message` keys

ts/types/Message.ts
@@ -5,39 +5,46 @@ export type Message = IncomingMessage | OutgoingMessage;
 
 export type IncomingMessage = {
   type: 'incoming';
+  // Required
   attachments: Array<Attachment>;
-  body?: string;
   conversationId: string;
-  decrypted_at?: number;
-  errors?: Array<any>;
-  flags?: number;
   id: string;
   received_at: number;
   sent_at: number;
+  timestamp: number;
+
+  // Optional
+  body?: string;
+  decrypted_at?: number;
+  errors?: Array<any>;
+  flags?: number;
   source?: string;
   sourceDevice?: number;
-  timestamp: number;
 } & Message4
 
 export type OutgoingMessage = {
   type: 'outgoing';
+
+  // Required
   attachments: Array<Attachment>;
-  body?: string;
   conversationId: string;
   delivered: number;
   delivered_to: Array<string>;
   destination: string; // PhoneNumber
   expirationStartTimestamp: number;
-  expires_at?: number;
-  expireTimer?: number;
   id: string;
   received_at: number;
-  recipients?: Array<string>; // Array<PhoneNumber>
   sent: boolean;
   sent_at: number;
   sent_to: Array<string>; // Array<PhoneNumber>
   synced: boolean;
   timestamp: number;
+
+  // Optional
+  body?: string;
+  expires_at?: number;
+  expireTimer?: number;
+  recipients?: Array<string>; // Array<PhoneNumber>
 } & Message4
 
 interface Message4 {

Bilb/desktop-test-rewrite

ts/types/Message.ts
@@ -8,32 +8,39 @@ export type Message
 
 export type IncomingMessage = Readonly<{
   type: 'incoming';
+  // Required
   attachments: Array<Attachment>;
+  id: string;
+  received_at: number;
+
+  // Optional
   body?: string;
   decrypted_at?: number;
   errors?: Array<any>;
   flags?: number;
-  id: string;
-  received_at: number;
   source?: string;
   sourceDevice?: number;
 } & SharedMessageProperties & Message4 & ExpirationTimerUpdate>;
 
 export type OutgoingMessage = Readonly<{
   type: 'outgoing';
+
+  // Required
   attachments: Array<Attachment>;
-  body?: string;
   delivered: number;
   delivered_to: Array<string>;
   destination: string; // PhoneNumber
   expirationStartTimestamp: number;
-  expires_at?: number;
-  expireTimer?: number;
   id: string;
   received_at: number;
-  recipients?: Array<string>; // Array<PhoneNumber>
   sent: boolean;
   sent_to: Array<string>; // Array<PhoneNumber>
+
+  // Optional
+  body?: string;
+  expires_at?: number;
+  expireTimer?: number;
+  recipients?: Array<string>; // Array<PhoneNumber>
   synced: boolean;
 } & SharedMessageProperties & Message4 & ExpirationTimerUpdate>;
 